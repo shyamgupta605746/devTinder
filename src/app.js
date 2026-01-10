dbc4bf868240081faa22b4a146f49011d9ed043b
shyam gupta <shyamgupta605746@gmail.com>
2026-01-10 15:20:49 +0530

    update query

--- src/app.js
+++ src/app.js
@@ -27,12 +27,43 @@ app.get("/user", async (req, res) => {
     }
 });
 
+app.get("/feed", async (req, res) => {
+    try{
+    const userEmail = req.body.emailId;
+    const user = await User.find({});
+    res.send(user);
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+app.delete("/user", async (req, res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
 app.get("/feed", async (req, res)=>{
     try{
         const users = await User.find({});
         res.send(users);
     }catch(err){
         res.status(400).send("something went wrong");
+    }
+});
+
+app.patch("/update", async (req, res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+         await User.findByIdAndUpdate({_id: userId}, data);
+         res.send("user updated sucessfully");
+    }catch(err){
+        res.status(500).send("something went wrong");
     }
 });
 
